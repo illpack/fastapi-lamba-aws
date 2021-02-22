ca7e53541c6af8df7f1948e199a330cb9cd11f57
Raf Rasenberg <rafrasenberg@gmail.com>
2021-02-22 19:19:19 +0100

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from mangum import Mangum
+from api_v1.api import router as api_router
+
+app = FastAPI(
+    title="My Awesome FastAPI app",
+    description="This is super fancy, with auto docs and everything!",
+    version="0.1.0",
+)
+
+
+@app.get("/ping", name="Healthcheck", tags=["Healthcheck"])
+async def healthcheck():
+    return {"Success": "Pong!"}
+
+
+app.include_router(api_router, prefix="/api/v1")
+
+handler = Mangum(app)
